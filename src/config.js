872560f879578c5f6d3269782a1234be1fbb6e0a
Ashok Advani <ashok.advani@gmail.com>
2018-12-24 22:14:03 -0800

    secondcommit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+    REGION: "us-west-2",
+    BUCKET: "notes-app-uploads-ba"
+    },
+    apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://10hvq5iv8f.execute/api.us-west-2.amazonaws.com/"
+    },
+    cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+   };
